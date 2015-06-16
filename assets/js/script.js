3c2258afeeb539bb10313efdf0f356458cd1dc68
SamuDev <smllms18@gmail.com>
2015-06-16 18:33:06 +0200

    ProvaCommit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var toShow = ["<div><h1>Amazing Photo 1</h1><img src=photo.jpg></div>",
-    "<div><h1>Amazing Photo 2</h1><img src='https://download.unsplash.com/photo-1429616588302-fec569e203ce'></div>",
-    "<div><h1>Amazing Photo 3</h1><img src='https://download.unsplash.com/photo-1429032021766-c6a53949594f'></div>"
+var toShow = ["<div><h1>Amazing Photo 1</h1><img src=photo.jpg /></div>",
+    "<div><h1>Amazing Photo 2</h1><img src='https://download.unsplash.com/photo-1429616588302-fec569e203ce'/></div>",
+    "<div><h1>Amazing Photo 3</h1><img src='https://download.unsplash.com/photo-1429032021766-c6a53949594f'/></div>"
 ];
 
 $("#menu li").hover(function (){

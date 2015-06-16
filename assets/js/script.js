cb849814a07451873f8155667d003f32c9368cfd
SamuDev <smllms18@gmail.com>
2015-06-16 18:24:59 +0200

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
+var toShow = ["<div><h1>Amazing Photo 1</h1><img src=photo.jpg></div>",
+    "<div><h1>Amazing Photo 2</h1><img src='https://download.unsplash.com/photo-1429616588302-fec569e203ce'></div>",
+    "<div><h1>Amazing Photo 3</h1><img src='https://download.unsplash.com/photo-1429032021766-c6a53949594f'></div>"
+];
+
 $("#menu li").hover(function (){
 	$(this).children("ul").slideToggle();
 });
 
-$("#menu li").children("ul").click(function(){
-    $("p").replaceWith( "<img src= photo.jpg>" );
+$(".class").click(function(){
+    var index = $(this).index();
+    console.log(index);
+
+    $("#container").html(toShow[index]);
 });

d955db185cca7a170835da6ec70ac1e8c7faea7b
SamuDev <smllms18@gmail.com>
2015-06-16 15:08:38 +0200

    newTask

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 $("#menu li").hover(function (){
 	$(this).children("ul").slideToggle();
 });
+
+$("#menu li").children("ul").click(function(){
+    $("p").replaceWith( "<img src= photo.jpg>" );
+});
